739c0469289df4e35fb671049b3ee9286a477c78
yevhensydorov <assid@ukr.net>
2018-01-14 13:40:10 +0000

    add two routes

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => res.send("Yay Node Girls"));
+
+app.get("/node", (req, res) => res.send("Its a Node route!"));
+
+app.get("/girls", (req, res) => res.send("Its a Girls route!"));
+
 
 app.listen(3000, () => console.log(`Server is listening on port 3000. Ready to accept requests!`));
