8d57b6ba95a8a36e44182d0a27672995de492b33
yevhensydorov <assid@ukr.net>
2018-01-14 16:13:02 +0000

    add write and read

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const express = require("express");
-const formidable = require("express-formidable");
-const app = express();
+const express = require("express"),
+			formidable = require("express-formidable"),
+			fs = require("fs"),
+			app = express();
+
+// fs.readFile(`${__dirname} /data/posts.json`, (error, file) => console.log(file));
+// fs.readFile(__dirname + '/data/posts.json', (error, file) => {
+// 	const parsedFile = JSON.parse(file);
+// 	console.log(file.toString());
+// });
+
 app.use(express.static("public"));
 app.use(formidable());
 
-app.post("/create-post", (req, res) => console.log(req.fields));
+app.post("/create-post", (req, res) => {
+	fs.readFile(__dirname + '/data/posts.json', (error, file) => {
+		const parsedFile = JSON.parse(file);
+		// console.log(file.toString());
+
+		parsedFile[Date.now()] = req.fields.blogpost;
+
+		fs.writeFile(__dirname + "/data/posts.json", JSON.stringify(parsedFile), error => {console.log(error)});
+	});
+	// console.log(req.fields.blogpost);
+});
 
 
 app.listen(3000, () => console.log(`Server is listening on port 3000. Ready to accept requests!`));
